e8c4889f986969c132b487984d599024193330c9
NgYuKang <ngyukang01@gmail.com>
2023-02-13 14:48:30 +0800

    feat: log in / register field error alert, log in invalid username/password alert

--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 # Create your views here.
@@ -14,3 +15,9 @@ class RegisterView(CreateView):
     template_name = "registration/signup.html"
     success_url = reverse_lazy('login')
     form_class = CustomUserCreationForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'You have successfully registered for an account. Please log in below.')
+        return response
+
