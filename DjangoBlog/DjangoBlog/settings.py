dc942b0808dc562c2c5001008028b936fe31e18e
NgYuKang <ngyukang01@gmail.com>
2023-02-16 13:06:50 +0800

    fix: proper casting from .env

--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=list, default=["0.0.0.0"])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default=["0.0.0.0", "127.0.0.1"])
 
 INTERNAL_IPS = [
     "127.0.0.1",
